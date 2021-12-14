f6e4764a1b73001fa4cfd661e6ccb6ff50962405
Kamillesvalgaard <70881285+Kamillesvalgaard@users.noreply.github.com>
2021-12-14 09:09:17 +0100

    Vildskud Child Theme

---
+++ hamilton-child/footer.php
@@ -0,0 +1,78 @@
+		</main>
+		
+		<footer class="site-footer section-inner footer">
+
+		<div>
+		<img class="nyhedsbrev" src="https://kamillesvalgaard.com/kea/3sem/vildskud/wp-content/themes/hamilton-child/nyhedsbrev.jpg" alt="collagenyhedsbrev">
+		
+		<script>(function() {
+	window.mc4wp = window.mc4wp || {
+		listeners: [],
+		forms: {
+			on: function(evt, cb) {
+				window.mc4wp.listeners.push(
+					{
+						event   : evt,
+						callback: cb
+					}
+				);
+			}
+		}
+	}
+})();
+</script><!-- Mailchimp for WordPress v4.8.6 - https://wordpress.org/plugins/mailchimp-for-wp/ --><form id="mc4wp-form-1" class="mc4wp-form mc4wp-form-591" method="post" data-id="591" data-name="Nyhedsbrev sign-up" ><div id="email" class="mc4wp-form-fields"><p>
+	
+	<label><h2 style="margin-top: -55vw; color: #333333">Vil du være med i fællesskabet hos VILDSKUD?</h2>
+		<input style="border: 3px solid #333333; position: relative" type="email" name="EMAIL" placeholder="Din e-mail" required />
+</label>
+</p>
+
+<p>
+	<input class="flex" id="submit" type="submit" value="Tilmeld" />
+</p></div><label style="display: none !important;">Leave this field empty if you&#8217;re human: <input type="text" name="_mc4wp_honeypot" value="" tabindex="-1" autocomplete="off" /></label><input type="hidden" name="_mc4wp_timestamp" value="1638874413" /><input type="hidden" name="_mc4wp_form_id" value="591" /><input type="hidden" name="_mc4wp_form_element_id" value="mc4wp-form-1" /><div class="mc4wp-response"></div></form><!-- / Mailchimp for WordPress Plugin -->
+			
+		<!--  -->
+		<!-- <div id="email">
+		<p>
+	<label>Email address: 
+		<input type="email" name="EMAIL" placeholder="Din e-mail" required />
+</label>
+</p>
+
+<p>
+	<input type="submit" value="Tilmeld" />
+</p> -->
+	<!-- <label>
+	
+    <input  type="email" name="EMAIL" required="" value="E-mail adresse">
+	</label>
+<p> 
+	<a href="https://kamillesvalgaard.com/kea/3sem/vildskud/tak/" ><input class="submit" type="submit" value="Tilmeld"></a>
+    
+</p> -->
+
+</div>
+		</div>
+		<div class="flex">
+			<a href="https://kamillesvalgaard.com/kea/3sem/vildskud/privatlivspolitik">Cookie- og privatlivspolitik</a>
+			<a href="https://kamillesvalgaard.com/kea/3sem/vildskud/refund_returns">Fragt og returnering</a>
+		</div>
+
+			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
+
+
+		</footer><!-- footer -->
+
+		<div id="social">
+				<a target="blank" href="https://www.facebook.com/Vildskud-Festival-MMD-Case-101791375693343/?ref=pages_you_manage">
+					<img src="https://kamillesvalgaard.com/kea/3sem/vildskud/wp-content/themes/hamilton-child/facebook@2x.png" alt="fbikon">
+				</a>
+				<a target="blank" href="https://www.instagram.com/vildskudfestival_mmdcase/">
+					<img src="https://kamillesvalgaard.com/kea/3sem/vildskud/wp-content/themes/hamilton-child/insta@2x.png"" alt="igikon">
+				</a>
+			</div>
+	    
+	    <?php wp_footer(); ?>
+	        
+	</body>
+</html>
